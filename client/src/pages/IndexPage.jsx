822c5d8d27bebd824eed8a8f36660c59bb7a7844
Cider8 <kajalkri2323@gmail.com>
2024-05-11 15:05:20 +0530

    Added homepage

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,42 @@
+export default function IndexPage(){
+    return(
+        <div >
+        <header className="p-4 flex justify-between">
+          <a href="" className="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+          </svg>
+          <span className="font-bold text-xl">airbnc</span>
+
+          </a>
+          <div className="flex gap-2 border border-gray-300 rounded-full p-2 px-4 shadow-md shadow-gray-300">
+            <div>Anywhere</div>
+            <div className="border border-l border-grey-300"></div>
+            <div>Any week</div>
+            <div className="border border-l border-grey-300"></div>
+            <div>Add guests</div>
+            <button className="bg-primary text-white p-1 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+
+            </button>
+          </div>
+          <div className="flex item-center gap-2 border border-gray-300 rounded-full p-2 px-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <div className="bg-gray-500 text-white rounded-full border-grey-500 overlow-hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 relative top-1">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+</svg>
+
+
+          </div>
+
+          </div>
+          
+        </header>
+      </div>
+    )
+}
