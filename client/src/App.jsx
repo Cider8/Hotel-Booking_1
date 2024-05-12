eed015003566c7a279c0e7e65c9d757bcd0e2aa9
Cider8 <kajalkri2323@gmail.com>
2024-05-12 13:40:57 +0530

    2nd commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import {Route, Routes} from "react-router-dom";
-import IndexPage from "./pages/IndexPage.jsx";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
 
 function App() {
   
 
   return (
     <Routes>
-    <Route index element={<IndexPage/>}/>
+      <Route path="/" element= {<Layout/>}>
+        <Route index element={<IndexPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Route>
   </Routes>
       
   )
